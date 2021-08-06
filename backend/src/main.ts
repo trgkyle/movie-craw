30954a3cc368d014c44d321b4f169a54d6b592b7
Truonghdpk <truongondaihoc@gmail.com>
2021-08-06 11:56:37 +0700

    update server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,13 +16,13 @@ async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
-  if (process.env.NODE_ENV === 'production' && process.env.PLATFORM !== 'HEROKU') {
-    console.log('Server running at : 443');
-    https.createServer(httpsOptions, server).listen(443);
-  } else {
-    console.log('Server running at : ' + (process.env.PORT || 3001));
-    http.createServer(server).listen(process.env.PORT || 3001);
-  }
+  // if (process.env.NODE_ENV === 'production' && process.env.PLATFORM !== 'HEROKU') {
+  //   console.log('Server running at : 443');
+  //   https.createServer(httpsOptions, server).listen(443);
+  // } else {
+  console.log('Server running at : ' + (process.env.PORT || 3001));
+  http.createServer(server).listen(process.env.PORT || 3001);
+  // }
 }
 
 bootstrap();
